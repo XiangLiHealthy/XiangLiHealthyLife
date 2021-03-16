51832f32e3179148f45c36c9774fdbfc9198d7fb
xiangbaosong <1406133062@qq.com>
2021-03-16 18:31:32 +0800

    add action recong test python code

---
+++ action_recongition/pythonProject/static_image.py
@@ -0,0 +1,28 @@
+import cv2
+import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+# For static images:
+with mp_pose.Pose(
+    static_image_mode=True, min_detection_confidence=0.5) as pose:
+  for idx, file in enumerate(file_list):
+    image = cv2.imread(file)
+    image_height, image_width, _ = image.shape
+    # Convert the BGR image to RGB before processing.
+    results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    if not results.pose_landmarks:
+      continue
+    print(
+        f'Nose coordinates: ('
+        f'{results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].x * image_width}, '
+        f'{results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].y * image_height})'
+    )
+    # Draw pose landmarks on the image.
+    annotated_image = image.copy()
+    # Use mp_pose.UPPER_BODY_POSE_CONNECTIONS for drawing below when
+    # upper_body_only is set to True.
+    mp_drawing.draw_landmarks(
+        annotated_image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+    cv2.imwrite('/tmp/annotated_image' + str(idx) + '.png', annotated_image)
